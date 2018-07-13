2b6a6b639762fb212db9915f43a586d6ac4c6024
skinpsv <skin_psv@hotmail.com>
2018-07-14 01:45:27 +0300

    gata timer

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const nume = "John";
 const prenume = "Smith";
+const view = {
+    show : function(target){
+        target.style.display = 'block';
+    },
+    hide : function(target){
+        target.style.display = 'none';
+    },
+    schimbaBg : function(){
+        if(document.body.style.backgroundColor == "yellow"){
+            document.body.style.backgroundColor = "blue"
+        } else {
+            document.body.style.backgroundColor = "yellow";
+        }
+    }
+}
+
 bgColor = "yellow";
 document.body.style.backgroundColor = bgColor;
+
 function foo(){
+    document.getElementById('timer').style.display='none';
+    document.getElementById('stop').style.display='none';
     const accountNume = document.getElementById('nume');
     let compunere = nume.toUpperCase();
     let compunere2 = prenume.toUpperCase();
@@ -11,16 +30,22 @@ function foo(){
     //.innerHTML = nume+' '+prenume;
   };
 const butonPlus = document.getElementById('plus');
-butonPlus.addEventListener('click', changeBg, false);
+const butonStop = document.getElementById('stop');
 
-function changeBg(){
-    if(document.body.style.backgroundColor == "yellow"){
-        document.body.style.backgroundColor = "green"
-    } else {
-        document.body.style.backgroundColor = "yellow";
-    }
+// butonPlus.style.display = 'none';
+butonPlus.addEventListener('click', changeBg, false);
+butonStop.addEventListener('click', cocos, false);
+function changeBg(event){
     
+    view.hide(event.target);
+    view.show(document.getElementById('timer'));
+    view.show(butonStop);
+    view.schimbaBg();
 }
-function cocos(){
-    console.log('test');
+function cocos(event){
+    view.hide(event.target);
+    view.hide(document.getElementById('timer'));
+    console.log(event.target);
+    view.show(butonPlus);
+    view.schimbaBg();
 }
