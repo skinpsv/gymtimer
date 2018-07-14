ae66daa89cb4a52b22e4d62d0d5955a2a3f997ad
skinpsv <skin_psv@hotmail.com>
2018-07-14 16:20:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const nume = "John";
-const prenume = "Smith";
+const prenume = "Smith"
+var interval = 499;
+var timer4 = document.getElementById('timer');
 const view = {
     show : function(target){
         target.style.display = 'block';
@@ -14,6 +16,37 @@ const view = {
             document.body.style.backgroundColor = "yellow";
         }
     }
+}
+
+// const timerO = {
+//     counter : function(x){
+//         setInterval(function(){
+//             x--;
+//             document.getElementById('timer').innerHTML = x;
+//         }, 1000);
+//     }
+// }
+
+// var timer1 = function(x){
+//     setInterval(function(){
+//         x--;
+//         document.getElementById('timer').innerHTML = x;
+//     }, 1000);
+// }
+
+
+console.log(interval);
+var timerO = {
+    interval : 500,
+    counter : function(){   
+        this.cisc = setInterval(function(){
+        interval--;
+        document.getElementById('timer').innerHTML = interval;
+        }
+        , 1000);
+    },
+    stopCount: function(){clearInterval(this.cisc);
+    interval = 500;}
 }
 
 bgColor = "yellow";
@@ -39,13 +72,18 @@ function changeBg(event){
     
     view.hide(event.target);
     view.show(document.getElementById('timer'));
+    timer4.innerHTML=interval;
     view.show(butonStop);
     view.schimbaBg();
+    //var interval = 400;
+    timerO.counter();
 }
 function cocos(event){
     view.hide(event.target);
-    view.hide(document.getElementById('timer'));
+    //view.hide(document.getElementById('timer'));
     console.log(event.target);
     view.show(butonPlus);
     view.schimbaBg();
+    // clearInterval(timerO.counter())
+    timerO.stopCount();
 }
